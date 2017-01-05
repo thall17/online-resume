345251c26e90cad90932638a25ce3ba4616a302e
Tim Hall <timhall3000@gmail.com>
2017-01-04 21:31:46 -0800

    Replace name and role in header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,15 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ var awesomeThoughts = "I am Tim and I am AWESOME!"
+ console.log(awesomeThoughts);
+
+ var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+ console.log (funThoughts);
+
+ var formattedName = HTMLheaderName.replace("%data%", "Tim");
+ var formattedRole = HTMLheaderRole.replace("%data%", "Developer");
+
+ $("#header").prepend(formattedRole);
+ $("#header").prepend(formattedName);
